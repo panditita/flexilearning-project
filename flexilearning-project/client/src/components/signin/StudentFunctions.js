cdd2d086c722312c322f200ac0b1f3d89b14cebd
Etza <topimorita@gmail.com>
2019-06-24 19:57:19 +0100

    fix

---
+++ flexilearning-project/client/src/components/signin/StudentFunctions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const register = (newStudent) => {
+	return axios
+		.post('/students/student-register', {
+			firstName: newStudent.firstName,
+			lastName: newStudent.lastName,
+			email: newStudent.email,
+			password: newStudent.password
+		})
+		.then((res) => {
+			console.log('Registered!');
+		});
+};
+
+export const login = (student) => {
+	return axios
+		.post('/students/student-login', {
+			email: student.email,
+			password: student.password
+		})
+		.then((res) => {
+			localStorage.setItem('studentToken', res.data);
+			return res.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
